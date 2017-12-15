f775a3e0a1c2a12760057f8e4c43e7fbbcf2777f
Brice Roberts <brkcco@gmail.com>
2017-12-15 14:51:32 -0600

    removed Roll

--- src/main/java/com/brkcco/starwars/diceroll/Dice.java
+++ src/main/java/com/brkcco/starwars/diceroll/Dice.java
@@ -3,50 +3,61 @@ package com.brkcco.starwars.diceroll;
 import com.brkcco.starwars.core.BaseDiceEntity;
 
 import javax.persistence.Entity;
-import javax.persistence.ManyToOne;
 import javax.validation.constraints.NotNull;
 
 
 @Entity
-public class Dice extends BaseDiceEntity  {
+public class Dice extends BaseDiceEntity {
   @NotNull
   private String diceName;
   private Integer diceSides;
-  private String diceFace;
+  private String[] diceFace;
   private String diceType;
 
-  @ManyToOne
-  private Roll roll;
 
-  protected Dice() {super();}
+  protected Dice() {
+    super();
+  }
 
-  public Dice(String diceName, Integer diceSides, String diceType, String diceFace) {
-    this();
-    this.diceFace = diceFace;
-
+  public Dice(String diceName, Integer diceSides, String diceType, String[] diceFace) {
     this.diceName = diceName;
     this.diceSides = diceSides;
     this.diceType = diceType;
+    this.diceFace = diceFace;
   }
 
-  public String getDiceName() {return diceName;  }
+  public String getDiceName() {
+    return diceName;
+  }
 
-  public void setDiceName(String diceName) {  this.diceName = diceName;  }
+  public void setDiceName(String diceName) {
+    this.diceName = diceName;
+  }
 
-  public Integer getDiceSides() {    return diceSides;  }
+  public Integer getDiceSides() {
+    return diceSides;
+  }
 
-  public void setDiceSides(Integer diceSides) { this.diceSides = diceSides;  }
+  public void setDiceSides(Integer diceSides) {
+    this.diceSides = diceSides;
+  }
 
-  public String getDiceFace() {    return diceFace;  }
+  public String[] getDiceFace() {
+    return diceFace;
+  }
 
-  public void setDiceFace(String diceFace) {    this.diceFace = diceFace;  }
+  public void setDiceFace(String[] diceFace) {
+    this.diceFace = diceFace;
+  }
 
-  public String getDiceType() {    return diceType;  }
+  public String getDiceType() {
+    return diceType;
+  }
 
-  public void setDiceType(String diceType) {    this.diceType = diceType;  }
+  public void setDiceType(String diceType) {
+    this.diceType = diceType;
+  }
 
-  public Roll getRoll() {    return roll;  }
 
-  public void setRoll(Roll roll) {    this.roll = this.roll;  }
 }
 
--- src/main/java/com/brkcco/starwars/diceroll/DiceRepository.java
+++ src/main/java/com/brkcco/starwars/diceroll/DiceRepository.java
@@ -8,14 +8,21 @@ import org.springframework.stereotype.Component;
 @Component
 public interface DiceRepository extends PagingAndSortingRepository<Dice, Long> {
   Dice findById(String id);
+
   Dice findByDiceName(String diceName);
 
-  default String findByRoll(String dice) {
-    return dice;
-  }
-  default String findByDiceType(String diceType){
+  default String findByDiceType(String diceType) {
     return diceType;
   }
+
+  default String findByResultType(String resultType) {
+    return resultType;
+  }
+
+  default String findByDiceFace(String diceFace) {
+    return diceFace;
+  }
+
 
 }
 
